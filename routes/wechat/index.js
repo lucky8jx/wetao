a80733948b04941083612bf8eb11a4fb348d7c28
lucky8jx <693150256@qq.com>
2017-06-04 00:31:01 +0800

    update wechat menu

--- routes/wechat/index.js
+++ routes/wechat/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import wechat from 'wechat';
+import WechatAPI from 'wechat-api';
+
 import serverConfig from '../../config/server.json';
 
 const router = express.Router();
+
+const wechatApi = new WechatAPI(serverConfig.wechat.appid, serverConfig.wechat.appsecret);
+const wechatMenu = serverConfig.wechat.menu;
 
 const config = {
   token: serverConfig.wechat.token,
@@ -19,6 +24,10 @@ router.use('/', wechat(config, (req, res, next) => {
   if (message.Content === 'diaosi') {
     res.reply('hehe')
   }
+
+  wechatApi.createMenu(wechatMenu, (err, result) => {
+		console.log(result);
+	});
 }));
 
 export default router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import session from 'express-session';
 import mongoose from 'mongoose';
 import passport from 'passport';
 import flash from 'connect-flash';
-import WechatAPI from 'wechat-api';
+
 
 import serverConfig from './config/server.json';
 import routes from './routes';
@@ -16,81 +16,7 @@ import apiV1 from './api/v1.0.0';
 import setUpPassport from './common/setuppassport';
 import wechat from './routes/wechat';
 
-const wechatApi = new WechatAPI(serverConfig.wechat.appid, serverConfig.wechat.appsecret);
-const wechatMenu = {
-	"button": [
-		{
-			"name": "领券优惠",
-			"sub_button": [
-				{
-					"type": "view",
-					"name": "优惠首页",
-					"url": "http://www.aiketao168.com"
-				},
-				{
-					"type": "view",
-					"name": "9.9元包邮",
-					"url": "http://aiketao168.com/index.php?r=nine&u=516697"
-				},
-				{
-					"type": "view",
-					"name": "居家日用",
-					"url": "http://aiketao168.com/index.php?r=l&cid=4&u=516697"
-				},
-				{
-					"type": "view",
-					"name": "超级人气榜",
-					"url": "http://aiketao168.com/index.php?r=p&u=516697"
-				},
-				{
-					"type": "view",
-					"name": "母婴",
-					"url": "http://aiketao168.com/index.php?r=l&cid=2&u=516697"
-				}
-			]
-		},
-		{
-			"name": "更多优惠",
-			"sub_button": [
-				{
-					"type": "view",
-					"name": "鞋包、配饰",
-					"url": "http://aiketao168.com/index.php?r=l&cid=5&u=516697"
-				},
-				{
-					"type": "view",
-					"name": "文体、车品",
-					"url": "http://aiketao168.com/index.php?r=l&cid=7&u=516697"
-				},
-				{
-					"type": "view",
-					"name": "数码、家电",
-					"url": "http://aiketao168.com/index.php?r=l&cid=8&u=516697"
-				},
-				{
-					"type": "view",
-					"name": "化妆品",
-					"url": "http://aiketao168.com/index.php?r=l&cid=3&u=516697"
-				},
-				{
-					"type": "view",
-					"name": "服装",
-					"url": "http://aiketao168.com/index.php?r=l&cid=1&u=516697"
-				}
-			]
-		},
-		{
-			"name": "限时抢购",
-			"sub_button": [
-				{
-					"type": "view",
-					"name": "咚咚抢",
-					"url": "http://www.aiketao168.com/index.php?r=ddq&u=516697"
-				}
-			]
-		}
-	]
-};
+
 
 const app = express();
 
@@ -126,6 +52,6 @@ app.use("/wechat", jsonParser, urlencodedParser, wechat);
 
 app.listen(serverConfig.port, serverConfig.ip, () => {
 	console.log("Server started on port " + serverConfig.port);
-	const result = wechatApi.createMenu(wechatMenu);
-	console.log(result);
+	
+
 });
